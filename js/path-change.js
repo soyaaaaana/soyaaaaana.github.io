b45a14ed506b4c508b1bf8ee9d3af4fb7eb86286
soyaaaaana <148342996+soyaaaaana@users.noreply.github.com>
2024-05-06 20:03:25 +0900

    Add files via upload

--- js/path-change.js
+++ js/path-change.js
@@ -1,8 +1,20 @@
-var path = location.href;
-var path2 = window.location.pathname.split("/");
-var regex = /index(\.html)?$/;
-if (location.protocol!="file:"&&path2[path2.length-1].match(regex))
+if  (location.protocol!="file:")
 {
-  path = path.replace(regex, "");
-  location.href = path;
+  var path = location.href;
+  var path2 = window.location.pathname.split("/");
+  var regex = /index(\.html)?$/;
+  var regex2 = /(\.html)?$/;
+  var changed = false;
+  if (path2[path2.length-1].match(regex))
+  {
+    path = path.replace(regex, "");
+    changed = true;
+  }
+  if (path2[path2.length-1].match(regex2))
+  {
+    path = path.replace(regex2, "");
+    changed = true;
+  }
+  if (changed)
+    location.href = path;
 }
