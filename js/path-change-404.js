5cd490ca38ce22572db04c5f6ca126b2775cd032
soyaaaaana <148342996+soyaaaaana@users.noreply.github.com>
2024-06-05 07:20:17 +0900

    Add files via upload

--- js/path-change-404.js
+++ js/path-change-404.js
@@ -1,10 +1,12 @@
 var changed = false;
 var path = location.href;
+/*
 if (path.slice(-1) == '/')
 {
   path = path.replace(/[\/]+$/, "");
   changed = true;
 }
+*/
 var path2 = window.location.pathname.split("/");
 if (path2[path2.length-1] == "index")
 {
